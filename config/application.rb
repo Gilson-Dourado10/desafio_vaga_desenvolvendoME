f2d2b6dc2a028b82fd8901534829358f8473e948
gilson dourado <gilsondourado10@gmail.com>
2022-10-25 06:23:57 -0300

    build:configure Rspec

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,14 @@ module DesafioVagaDesenvolvendome
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: true,
+                       helper_specs: true,
+                       routing_specs: false,
+                       request_specs: false
+      g.factory_bot dir: "spec/factories"
+      end
   end
 end
